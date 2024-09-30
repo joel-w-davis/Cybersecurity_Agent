024c5d3f5e9c0ce6c4dc0ae5d9d29d72b130c0a3
joel-w-davis50 <98930173+joel-w-davis50@users.noreply.github.com>
2024-09-30 10:57:21 -0400

    Initial commit7

--- templates/frontend.py
+++ templates/frontend.py
@@ -2,7 +2,8 @@ import streamlit as st
 import requests
 
 # Set the URL of the Flask API
-API_URL = "https://cybersecurity-agent-y77swv41h-joel-davis-projects-7ca38eef.vercel.app"
+API_URL = "https://cybersecurity-agent.vercel.app/"
+
 
 # Title of the Streamlit app
 st.title("Cybersecurity Agent Q&A")
